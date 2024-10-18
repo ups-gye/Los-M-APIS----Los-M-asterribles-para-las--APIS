5665ca58983d10ada6c426bbe054d9e76c757112
paulov <paulos_50@hotmail.com>
2024-10-17 19:54:15 -0500

    update apis

---
+++ reservas-frontend/src/apis/usersApi.ts
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+const usersApi = axios.create({
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api/v1/users`
+})
+
+
+usersApi.interceptors.request.use((config) => {
+  const token = sessionStorage.getItem('token')
+  if (token) {
+    config.headers['Authorization'] = token
+  }
+  return config
+})
+
+export { usersApi }
